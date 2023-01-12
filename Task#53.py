abcaa310e041717b27e3a290c29ea9be233c59bd
Roman Kozhin <kozhin.rm@gmail.com>
2023-01-12 04:23:45 +0300

    Вычислить число c заданной точностью

---
+++ Task#53.py
@@ -0,0 +1,21 @@
+# Вычислить число c заданной точностью d
+'''
+Пример:
+- при d = 0.001, π = 3.141    10^{-1} ≤ d ≤10^{-10}
+'''
+
+
+import math
+from decimal import Decimal
+
+num = float(input("Введите точность числа в диапазоне [0.1, 1e-10] >>> "))
+
+s = str(num)
+if '.' not in s:
+    d = abs(int(s[s.index('1e') + 2 :]))
+else:
+    d = len(s[s.index('.') + 1:])
+
+
+res = format(math.pi, f'.{d}f')
+print(res)
